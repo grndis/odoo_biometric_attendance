837f0b1870de998a5ce6b7f8e02907987d7f988c
Grandis SYF <gra@dr.com>
2025-10-02 11:55:12 +0700

    feat(manifest): Update version to 19.0.1.2.2

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Biometric Device Integration",
-    "version": "18.0.1.2.2",
+    "version": "19.0.1.2.2",
     "category": "Human Resources",
     "summary": "Integrating Biometric Device (Model: ZKteco uFace 202) With HR"
     "Attendance (Face + Thumb)",
